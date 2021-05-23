6d1c3f356c9e21426a35efcaa45ccab273e676f8
irene-gon <63265021+irene-gon@users.noreply.github.com>
2021-05-23 21:12:47 +0200

    subida con ficheros faltantes

---
+++ src/app/interfaces/person.ts
@@ -0,0 +1,12 @@
+export interface Person {
+        nombre: String,
+        apellidos: String,
+        edad: any,
+        dni: String,
+        cumpleanos: Date,
+        color: String,
+        sexo: String,
+        notas: String
+      }
+
+
